f9ba1a9566228f68f2994b1cef0917585a24fdd4
huyphan <phangiahuy2001@gmail.com>
2023-08-31 15:25:20 +0700

    Prevent sending notifications between users in blocking list

--- back-end/src/main/java/com/huyphan/services/notification/receiverresolver/AddPostNotificationReceiverResolver.java
+++ back-end/src/main/java/com/huyphan/services/notification/receiverresolver/AddPostNotificationReceiverResolver.java
@@ -2,8 +2,12 @@ package com.huyphan.services.notification.receiverresolver;
 
 import com.huyphan.models.Post;
 import com.huyphan.models.User;
+import com.huyphan.repositories.NotificationRepository;
 import com.huyphan.services.notification.payload.AddPostNotificationPayload;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +17,13 @@ public class AddPostNotificationReceiverResolver implements
     @Override
     public List<User> resolve(AddPostNotificationPayload notificationPayload) {
         Post post = notificationPayload.getPost();
-        User owner = post.getOwner();
-        return owner.getFollowers().stream().toList();
+        User postOwner = post.getUser();
+        Set<User> postOwnerFollowers = postOwner.getFollowers();
+        Set<User> postOwnerBlocking = postOwner.getBlocking();
+        Set<User> blockingPostOwner = postOwner.getBlockedBy();
+        postOwnerFollowers.removeAll(postOwnerBlocking);
+        postOwnerFollowers.removeAll(blockingPostOwner);
+
+        return postOwnerFollowers.stream().toList();
     }
 }
