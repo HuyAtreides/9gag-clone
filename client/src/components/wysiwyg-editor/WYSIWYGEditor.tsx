7b36bee2e790c3d72c1262476a84ed609449ed04
huyphan <phangiahuy2001@gmail.com>
2024-01-07 21:19:51 +0700

    Adjust UI

--- client/src/components/wysiwyg-editor/WYSIWYGEditor.tsx
+++ client/src/components/wysiwyg-editor/WYSIWYGEditor.tsx
@@ -4,18 +4,19 @@ import { useEffect, useRef } from 'react';
 interface Props {
   readonly onChange?: (value: string) => void;
   readonly height?: string;
+  readonly initialValue?: string;
 }
 
-const WYSIWYGEditor: React.FC<Props> = ({ onChange, height = '50vh' }) => {
+const WYSIWYGEditor: React.FC<Props> = ({ onChange, initialValue, height = '50vh' }) => {
   const editorRef = useRef<Editor | null>(null);
 
   useEffect(() => {
     const editor = editorRef.current;
 
-    if (editor) {
+    if (!initialValue && editor) {
       editor.getInstance().setHTML('');
     }
-  }, []);
+  }, [initialValue]);
 
   const contentChanged = () => {
     const editor = editorRef.current;
@@ -34,6 +35,7 @@ const WYSIWYGEditor: React.FC<Props> = ({ onChange, height = '50vh' }) => {
       initialEditType='wysiwyg'
       onKeyup={contentChanged}
       useCommandShortcut
+      initialValue={initialValue}
       usageStatistics={false}
     />
   );
